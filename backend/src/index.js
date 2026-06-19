8aaac60886c70d9ed55af66b38f458921d3619a6
Aaditya <patilaadi2006@gmail.com>
2026-06-20 01:12:27 +0530

    connect to mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,21 @@
 import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
+import 'dotenv/config'
 
+connectDB()
+.then(() => {
+  app.on("error", (error) => {
+    console.log(error);
+    throw error
+  })
 
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server Running on Port ${process.env.PORT}`);
+  })
 
-app.listen(8000, () => {
-  console.log("Server Running on Port 8000");
 })
+.catch((err) => {
+  console.log("MONGODB Connection Failed!!");
+})
+
+
