928313c3e1ada249c4a626747418e4ec1b2faef3
Aaditya <patilaadi2006@gmail.com>
2026-06-21 02:21:06 +0530

    add reusable function to get note by id and verify owner

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -4,6 +4,25 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { options } from "../utils/httpOptions.js";
 import { Note } from "../models/note.model.js";
 
+const getNoteByIdAndVerifyOwner = async(noteId, userId) => {
+
+  if(!noteId){
+    throw new ApiError(400, "Note ID is required")
+  }
+
+  const note = await Note.findById(noteId);
+
+  if(!note){
+    throw new ApiError(404, "Note not found")
+  }
+
+  if(note.owner.toString() !== userId.toString()){
+    throw new ApiError(403, "Access Denied")
+  }
+
+  return note
+}
+
 
 const createNote = asyncHandler(async(req, res) => {
 
@@ -42,6 +61,17 @@ const getNotes = asyncHandler(async(req, res) => {
     .status(200)
     .json(
       new ApiResponse(200, notes, "Notes Successfully Fetched!!")
+    )
+})
+
+const getNoteById = asyncHandler(async(req, res) => {
+  
+  const note = getNoteByIdAndVerifyOwner(req.params.id. req.user._id);
+
+  return res 
+    .status(200)
+    .json(
+      new ApiResponse(200, note, "Note fetched successfully")
     )
 })
 
