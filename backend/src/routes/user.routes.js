6113589f8662060a3a9fcc9eb9e9c33428e8870b
Aaditya <patilaadi2006@gmail.com>
2026-06-21 00:09:39 +0530

    update user routes with update avatar and refresh access token routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {upload} from "../middlewares/multer.middleware.js";
-import { registerUser, loginUser, getCurrentUser, logoutUser, changePassword, updateDetails } from "../controllers/user.controller.js";
+import { registerUser, loginUser, getCurrentUser, logoutUser, changePassword, updateDetails, updateAvatar, refreshAccessToken } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -15,15 +15,28 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser)
 
+router.route("/refresh-token").post(refreshAccessToken)
 
 //secure routes
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 
 router.route("/logout").post(verifyJWT, logoutUser)
 
-router.route("change-password").patch(verifyJWT, changePassword)
+router.route("/change-password").patch(verifyJWT, changePassword)
 
-router.route("update-details").patch(verifyJWT, updateDetails)
+router.route("/update-details").patch(verifyJWT, updateDetails)
+
+router.route("/update-avatar")
+  .patch(
+    verifyJWT,   
+    upload.fields([{
+    name: "avatar",
+    maxCount: 1
+    }]),
+    updateAvatar
+  )
+
+
 
 
 export default router
